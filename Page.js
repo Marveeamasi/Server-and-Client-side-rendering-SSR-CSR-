bbcfc4c537fabf281b406977b824b3bef2eb2abd
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-12-06 06:35:02 +0100

    Create Page.js

---
+++ Page.js
@@ -0,0 +1,14 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.example.com/data')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return <div>{data ? JSON.stringify(data) : 'Loading...'}</div>;
+}
